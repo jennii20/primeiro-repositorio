c4eb8d928cf7fdda7e07050047969e9c85488331
jennii20 <jennifer.fernanda699@gmail.com>
2022-09-15 21:32:39 -0300

    aula javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Tipos primitivos
+ * Variáveis no JavaScript
+ * Operadores matemáticos do JavaScript
+ * 
+ */
+
+// serve para escrever mensagens no console/no terminal 
+
+//String = textos, uma cadeia de caracteres
+//Number = números qualquer tipo de numeros
+//boolean/ boolean = representa somente dois valores verdadeiro ou falso
+console.log('Hello World!')
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+
+//Variáveis
+let mensagem = 'Minha mensagem' //declaração da variavel
+
+console.log(mensagem)
+
+mensagem = 'novo texto'
+console.log(mensagem)
